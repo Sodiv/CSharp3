8dbdf95b4b612daf5bcb18ef46d9b5f3f51cc568
Дмитрий <sodiv@mail.ru>
2018-11-03 23:41:43 +0500

    ee

---
+++ ConvertCSVtoTXT/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace ConvertCSVtoTXT
+{
+    class Program
+    {
+        class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Univercity { get; set; }
+            public string Faculty { get; set; }
+            public string Department { get; set; }
+            public string Age { get; set; }
+            public string Course { get; set; }
+            public string Group { get; set; }
+            public string City { get; set; }
+        }
+
+        static BlockingCollection<Student> students = new BlockingCollection<Student>();
+
+        static void Main(string[] args)
+        {
+            ReadCSV();
+        }
+
+        private static void ReadCSV()
+        {
+            StreamReader sr = new StreamReader();
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    students.Add(new Student { FirstName = s[0], LastName = s[1], Univercity = s[2], Faculty = s[3], Department = s[4],
+                        Age = s[5], Course = s[6], Group = s[7], City = s[8] });
+                }
+                catch { }
+            }
+        }
+    }
+}
--- MathThread/Program.cs
+++ MathThread/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Threading;
 
 namespace MathThread
 {
     class Program
     {
+        static int n;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите целое число: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Thread threadFactorial = new Thread(Factorial);
+            Thread threadSumm = new Thread(Summ);
+            threadFactorial.Start();
+            threadSumm.Start();
+            Console.WriteLine("Ждем оконачания работы потоков");
+            Console.ReadKey();
+        }
+
+        static void Factorial()
+        {
+            long res = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                res *= i;
+            }
+            Console.WriteLine($"Факториал числа {n}: {res}");
+        }
+
+        static void Summ()
+        {
+            int res = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                res += i;
+            }
+            Console.WriteLine($"Сумма всех чисел до числа {n}: {res}");
         }
     }
 }
