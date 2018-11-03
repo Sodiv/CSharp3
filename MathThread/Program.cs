d8b9858f67ec36824acc1f553c4f67e68ff831be
Дмитрий <sodiv@mail.ru>
2018-11-03 22:06:06 +0500

    Добавьте файлы проекта.

---
+++ MathThread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
