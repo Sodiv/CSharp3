a6cfdea7788e3343f885c40cb35f93f131b5bee5
Дмитрий <sodiv@mail.ru>
2018-11-04 19:47:55 +0500

    Завершено

--- ConvertCSVtoTXT/Program.cs
+++ ConvertCSVtoTXT/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.IO;
+using System.Threading;
 
 namespace ConvertCSVtoTXT
 {
@@ -23,22 +24,42 @@ namespace ConvertCSVtoTXT
 
         static void Main(string[] args)
         {
-            ReadCSV();
+            Thread threadCSV = new Thread(ReadCSV);
+            Thread threadTXT = new Thread(WriteToTXT);
+            threadCSV.Start();
+            threadTXT.Start();
+            threadTXT.Join();
+            Console.WriteLine("Завершено");
+            Console.ReadKey();
         }
 
         private static void ReadCSV()
         {
-            StreamReader sr = new StreamReader();
+            StreamReader sr = new StreamReader("../../../students_4.csv");
             while (!sr.EndOfStream)
             {
                 try
                 {
                     string[] s = sr.ReadLine().Split(';');
+                    
                     students.Add(new Student { FirstName = s[0], LastName = s[1], Univercity = s[2], Faculty = s[3], Department = s[4],
                         Age = s[5], Course = s[6], Group = s[7], City = s[8] });
                 }
-                catch { }
+                catch { }                
             }
+            students.CompleteAdding();
+            sr.Close();
+        }
+
+        private static void WriteToTXT()
+        {
+            StreamWriter sw = new StreamWriter("student.txt");
+            foreach(var item in students.GetConsumingEnumerable())
+            {
+                sw.WriteLine($"{item.FirstName} {item.LastName} {item.Univercity} {item.Faculty} {item.Department} " +
+                    $"{item.Age} {item.Course} {item.Group} {item.City}");
+            }
+            sw.Close();
         }
     }
 }
